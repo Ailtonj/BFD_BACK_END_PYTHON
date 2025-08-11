13c56a30be841b3dabb194ae6791563b130445b8
Ailtonj <ailtonj.almeida@gmail.com>
2025-08-11 16:43:04 -0300

    Delete exerc_6.py

--- exerc_6.py
+++
@@ -1,9 +0,0 @@
-
-#Entrada de dados
-palavra_1 = input("Digite a primeira palavra: ")
-palavra_2 = input("Digite a segunda palavra: ")
-palavra_3 = input("Digite a terceira palavra: ")
-palavra_4 = input("Digite a quarta palavra: ")
-
-# Saaída de dados
-print(palavra_1, palavra_2, palavra_3, palavra_4)
