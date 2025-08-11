915e38be9b6a0706aaf1690d5507f3454c00c199
Ailtonj <ailtonj.almeida@gmail.com>
2025-08-11 16:44:00 -0300

    Delete exerc_9.py

--- exerc_9.py
+++
@@ -1,7 +0,0 @@
-
-#Entrada de dados
-nome_produto = input("Digite o nome do produto: ")
-preco_produto = float(input("Digite o preço do produto: "))
-
-# Saída de dados
-print(f"O produto {nome_produto} custa R$ {preco_produto}.")
